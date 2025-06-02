5add03de118a3f18ad218c08d4862577e526a9c7
Josue Adrian Sosa Cruz <jasosac@unjbg.edu.pe>
2025-06-02 12:29:03 -0500

    Actualiza ejercicio 1 en C++: agrega validación de calificaciones

--- ejercicio_1.cpp
+++ ejercicio_1.cpp
@@ -3,18 +3,34 @@ using namespace std;
 
 int main() {
     float cal1, cal2, cal3, promedio;
-    cout << "Ingrese la primera calificacion: ";
+
+    cout << "Ingresa la primera calificación: ";
     cin >> cal1;
-    cout << "Ingrese la segunda calificacion: ";
+    if (cal1 < 0 || cal1 > 100) {
+        cout << "Calificación inválida." << endl;
+        return 1;
+    }
+
+    cout << "Ingresa la segunda calificación: ";
     cin >> cal2;
-    cout << "Ingrese la tercera calificacion: ";
+    if (cal2 < 0 || cal2 > 100) {
+        cout << "Calificación inválida." << endl;
+        return 1;
+    }
+
+    cout << "Ingresa la tercera calificación: ";
     cin >> cal3;
+    if (cal3 < 0 || cal3 > 100) {
+        cout << "Calificación inválida." << endl;
+        return 1;
+    }
+
     promedio = (cal1 + cal2 + cal3) / 3;
-    cout << "Promedio: " << promedio << endl;
+
     if (promedio >= 70) {
-        cout << "El alumno APRUEBA el curso." << endl;
+        cout << "El alumno aprueba con promedio de " << promedio << endl;
     } else {
-        cout << "El alumno REPRUEBA el curso." << endl;
+        cout << "El alumno reprueba con promedio de " << promedio << endl;
     }
 
     return 0;
